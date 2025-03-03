16a659e9bae03702aa158cb2f9a44bb5ad2c874f
KaanSoyaslan <kwlstds@gmail.com>
2025-03-03 21:47:03 +0300

    wip: add word

---
+++ Assets/Scripts/AddWord.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+public class AddWord : MonoBehaviour
+{
+
+    [SerializeField] private TMP_InputField firstIF;
+    [SerializeField] private TMP_InputField secondIF;
+    void Start()
+    {
+        
+    }
+
+    public void AddNewWord()
+    {
+
+        if (true)
+        {
+
+        }
+    }
+}
